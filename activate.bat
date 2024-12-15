5c6f40f0b8655a72c513005e3dcccc1f03168511
Randy Marsh <46230578+Randy420Marsh@users.noreply.github.com>
2024-12-15 20:27:52 +0200

    Update activate.bat
    fixed some unneeded lines

--- activate.bat
+++ activate.bat
@@ -6,10 +6,11 @@ cd %current_path%
 
 setlocal enabledelayedexpansion
 
-set python="C:\\Users\\John\\AppData\\Local\\Programs\\Python\\Python310\\python.exe"
+REM Not needed
+REM set python="C:\\Users\\%USERPROFILE%\\AppData\\Local\\Programs\\Python\\Python310\\python.exe"
+REM set python3="C:\\Users\\%USERPROFILE%\\AppData\\Local\\Programs\\Python\\Python310\\python.exe"
 
-set python3="C:\\Users\\John\\AppData\\Local\\Programs\\Python\\Python310\\python.exe"
-
-set "PATH=%VIRTUAL_ENV%\Scripts;C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.42.34433\bin\Hostx64\x64;%PATH%"
+REM set "PATH=%VIRTUAL_ENV%\Scripts;C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.42.34433\bin\Hostx64\x64;%PATH%"
+REM Not needed
 
 IF exist ./venv (cmd /k call .\venv\scripts\activate.bat)  ELSE (cmd /k python -m venv venv && cmd /k call .\venv\scripts\activate.bat)
